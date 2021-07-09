d72a2ba9af51e07da08a61ddb765e075da3408cd
Marat Oshmiansky <maratical@gmail.com>
2021-07-09 08:41:20 -0700

    update articles controller index method

--- Gemfile
+++ Gemfile
@@ -38,5 +38,3 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'http'
-
-gem 'pexels'
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,6 @@
 class ArticlesController < ApplicationController
+  def index
+    response = HTTP.get("https://newsapi.org/v2/everything?q=keyword")  # API key is in the Insomnia header
+    render json: response
+  end
 end
